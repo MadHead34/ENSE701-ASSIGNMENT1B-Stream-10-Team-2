b07ed88d37e86b6b8274da02c67e9f07ae9db1a7
The-Doggy <frogboynz@gmail.com>
2020-05-21 14:18:48 +1200

    Searching mostly complete and working, may still be a few changes before I'm happy with it.

--- SEER/Models/SeedData.cs
+++ SEER/Models/SeedData.cs
@@ -2,6 +2,7 @@
 using LexicalAnalyzer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SEER.Data;
 using System;
 using System.Collections.Generic;
@@ -26,30 +27,69 @@ namespace SEER.Models
 
             try
             {
+                // TODO: Setup switches or something with a foreach loop over the entry.Tags keys
                 using StreamReader sr = new StreamReader("input.txt", Encoding.Default);
                 BibtexFile file = BibtexImporter.FromString(sr.ReadToEnd().Replace("'", "", StringComparison.Ordinal));
 
                 foreach (BibtexEntry entry in file.Entries)
                 {
+                    string? title = null, author = null, journal = null, pageNum = null, doi = null;
+                    int? year = null, volume = null, number = null;
+                    foreach (KeyValuePair<string, string> pair in entry.Tags)
+                    {
+                        string replaced = pair.Value.Replace("{", "", StringComparison.Ordinal).Replace("}", "", StringComparison.Ordinal).Replace("\\", "", StringComparison.Ordinal);
+                        switch (pair.Key)
+                        {
+                            case "title":
+                                title = replaced;
+                                break;
+                            case "author":
+                                author = replaced;
+                                break;
+                            case "journal":
+                            case "publisher":
+                                journal = replaced;
+                                break;
+                            case "pages":
+                                pageNum = replaced;
+                                break;
+                            case "doi":
+                                doi = replaced;
+                                break;
+                            case "year":
+                                year = int.Parse(replaced);
+                                break;
+                            case "volume":
+                                volume = int.Parse(replaced);
+                                break;
+                            case "number":
+                                number = int.Parse(replaced);
+                                break;
+                            default:
+                                Console.Error.WriteLine($"Unknown key {pair.Key} with value {pair.Value} in bibtex entry.");
+                                break;
+                        }
+                    }
                     context.BibliographicReference.Add(
                         new BibliographicReference
                         {
-                            Title = entry.Tags["title"],
-                            Author = entry.Tags["author"],
-                            Year = int.Parse(entry.Tags["year"]),
-                            Source = entry.Tags["journal"],
-                            Volume = int.Parse(entry.Tags["volume"]),
-                            Number = int.Parse(entry.Tags["number"]),
-                            PageNumbers = entry.Tags["pages"],
-                            DOI = entry.Tags["doi"]
+                            // TODO: Make these not show up on user side when unknown? Probably not able to do that here lol
+                            Title = title == null ? "Unknown" : title,
+                            Author = author == null ? "Unknown" : author,
+                            Source = journal == null ? "Unknown" : journal,
+                            PageNumbers = pageNum == null ? "Unknown" : pageNum,
+                            DOI = doi == null ? "Unknown" : doi,
+                            Year = year.GetValueOrDefault(),
+                            Volume = volume.GetValueOrDefault(),
+                            Number = number.GetValueOrDefault()
                         }
                     );
                 }
                 context.SaveChanges();
             }
-            catch (MatchException ex)
+            catch (Exception ex)
             {
-
+                Console.Error.WriteLine(ex.Message);
             }
         }
     }
--- SEER/Pages/BibliographicReferences/Index.cshtml.cs
+++ SEER/Pages/BibliographicReferences/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SEER.Data;
 using SEER.Models;
@@ -19,11 +20,57 @@ namespace SEER
             _context = context;
         }
 
-        public IList<BibliographicReference> BibliographicReference { get;set; }
-
+        public IList<BibliographicReference> BibliographicReference { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchTitle { get; set; }
+        public SelectList Sources { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchSource { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchAuthor { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchDOI { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int SearchYearStart { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int SearchYearEnd { get; set; }
         public async Task OnGetAsync()
         {
-            BibliographicReference = await _context.BibliographicReference.ToListAsync();
+            // Use LINQ to get list of sources.
+            IQueryable<string> sourceQuery = from r in _context.BibliographicReference
+                                             orderby r.Source
+                                             select r.Source;
+
+            var references = from r in _context.BibliographicReference
+                            select r;
+
+            if(!string.IsNullOrEmpty(SearchTitle))
+            {
+                references = references.Where(s => s.Title.Contains(SearchTitle));
+            }
+
+            if (!string.IsNullOrEmpty(SearchSource))
+            {
+                references = references.Where(x => x.Source == SearchSource);
+            }
+
+            if(!string.IsNullOrEmpty(SearchAuthor))
+            {
+                references = references.Where(s => s.Author.Contains(SearchAuthor));
+            }
+
+            if(!string.IsNullOrEmpty(SearchDOI))
+            {
+                references = references.Where(s => s.DOI.Contains(SearchDOI));
+            }
+
+            if(SearchYearStart >= 1970 && SearchYearStart <= 2020 && SearchYearEnd >= 1970 && SearchYearEnd <= 2020)
+            {
+                references = references.Where(s => s.Year >= SearchYearStart && s.Year <= SearchYearEnd);
+            }
+
+            Sources = new SelectList(await sourceQuery.Distinct().ToListAsync());
+            BibliographicReference = await references.ToListAsync();
         }
     }
 }
