69132ad7152bff2e4370e481cd282f8f7742affc
The-Doggy <frogboynz@gmail.com>
2020-06-04 23:12:58 +1200

    Added tables for analyst/moderator

---
+++ SEER/Migrations/20200604110913_CreateArticleTables.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SEER.Migrations
+{
+    public partial class CreateArticleTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ SEER/Models/InitialArticle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEER.Models
+{
+    public class InitialArticle
+    {
+        public int ID { get; set; } // Primary Key
+        [Required]
+        [Display(Order = -1)]
+        public string Title { get; set; }
+        [Display(Order = 0)]
+        [Required]
+        public string Author { get; set; }
+        [Display(Order = 0)]
+        [Required]
+        public string Source { get; set; } // This is the source of where the reference came from e.g. journal/web address for articles/web pages and publisher for books
+        [Required]
+        [Display(Prompt = "2020", Order = 0)]
+        [Range(1970, 2020)]
+        public int Year { get; set; }
+        [Required]
+        [Display(Order = 0)]
+        [Url(ErrorMessage = "Enter a Url")]
+        public string DOI { get; set; }
+    }
+}
